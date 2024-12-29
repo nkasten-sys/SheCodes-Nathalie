2ea9e95669acfa10638d76d381ba055369fdeeda
nkasten-sys <nkasten@bluewin.ch>
2024-12-29 17:18:14 +0100

    My project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayTemperature(response) {
+function showTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
@@ -11,10 +11,10 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "fb9717fd9t00e1ao1ceeb9082c6f7345";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(showTemperature);
 }
 
 function formatDate(date) {
@@ -37,7 +37,7 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
 
   let formattedDay = days[day];
@@ -51,3 +51,56 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  moscow: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+
+city = city.toLowerCase();
+if (weather[city] !== undefined) {
+  let humidity = weather[city].humidity;
+  let temperature = weather[city].temp;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+  alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
+
+function showyCityAndTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let city = response.data.city;
+
+  let headingElement = document.querySelector("#current-city");
+  let celciusElement = document.querySelector("current-temperature-value");
+
+  headingElement.innerHTML = city;
+  celciusElement.innerHTML = `${temperature}°C`;
+}
+
+let city = "Sydney";
+axios.get(apiUrl).then(showCityAndTemperature);
